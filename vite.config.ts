40d279b237b16c9e5372ea9182545a4cd78ecf57
dankostecki <iluzjadk@wp.pl>
2025-06-06 17:03:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wykroczenie/', // ← DODAJ TO
+  base: '/wykroczenie/',
   server: {
     port: 3000,
   },
   build: {
     outDir: "dist",
+  },
+  css: {
+    postcss: null, // Wyłącz PostCSS
   },
 });
